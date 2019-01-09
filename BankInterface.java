0d2c1d786e8ce1bde467fee6c67a45b1e14824d8
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-01-09 11:33:15 +0000

    Update BankInterface.java

--- BankInterface.java
+++ BankInterface.java
@@ -1,6 +1,6 @@
-
+	
 public interface BankInterface {
-	 void deposit();  // public abstract is default of interface method
-	abstract void withdrow();  // public abstract is default of interface method
+	 	 	void deposit();      // public abstract is default of interface method
+	       abstract void withdrow();     // public abstract is default of interface method
 	public abstract void calcinterest(); // same effect as above methods.
 }	
