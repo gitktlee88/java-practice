5f1c8935ebe96fb01eff904ad4693704d12b7208
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-01-09 10:54:30 +0000

    Update UsingFixedThreadPool.java

--- UsingFixedThreadPool.java
+++ UsingFixedThreadPool.java
@@ -1,3 +1,10 @@
+/*
+ * The Java ExecutorService is a construct that allows you to pass a task to be executed by a thread asynchronously. 
+ * The executor service creates and maintains a reusable pool of threads for executing submitted tasks. 
+ * The service also manages a queue, which is used when there are more tasks than the number of threads in the pool 
+ * and there is a need to queue up tasks until there is a free thread available to execute the task.
+ */
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 //import LoopTaskA;
