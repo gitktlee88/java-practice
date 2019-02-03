01838ee575162a7bed1cfb2236d24cd11a74f742
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-02-03 20:58:42 +0000

    Update futureIsDoneExample.java

--- thread/futureIsDoneExample.java
+++ thread/futureIsDoneExample.java
@@ -1,3 +1,46 @@
+//Following is a simple example of Future and Callable -
+
+import java.util.concurrent.*;
+
+public class FutureAndCallableExample {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Callable<String> callable = () -> {
+            // Perform some computation
+            System.out.println("Entered Callable");
+            Thread.sleep(2000);
+            return "Hello from Callable";
+        };
+
+        System.out.println("Submitting Callable");
+        Future<String> future = executorService.submit(callable);
+
+        // This line executes immediately
+        System.out.println("Do something else while callable is getting executed");
+
+        System.out.println("Retrieve the result of the future");        
+        // Future.get() blocks until the result is available
+        String result = future.get();
+        System.out.println(result);
+
+        executorService.shutdown();
+    }
+
+}
+
+# Output
+Submitting Callable
+Do something else while callable is getting executed
+Retrieve the result of the future
+Entered Callable
+Hello from Callable
+
+
+
+// Note that, the get() method blocks until the task is completed. 
+// The Future API also provides an isDone() method to check whether the task is completed or not -
+
 import java.util.concurrent.*;
 
 public class FutureIsDoneExample {
