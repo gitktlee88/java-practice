4ba7747d5e8f32884f1d4bb5538c06e552da039b
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-02-05 14:38:49 +0000

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -24,3 +24,69 @@ public class FizzBuzz {
 	}
 
 }
+
+
+
+
+/////// String version /////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class FizzBuzz {
+	// static int[] intArray;
+
+	public static void main(String args[]) throws IOException {
+		//enter the path and dirname from keyboard 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        System.out.println("Enter star number:"); 
+        String start=br.readLine();
+        int snum = Integer.valueOf(start);
+       
+//       System.out.println("Enter end number:"); 
+//       String end=br.readLine();
+//       int endnum = Integer.valueOf(end);
+       
+       //this console read works on command line not on IDE(Eclipse)
+//		  System.out.println("Enter end number: ");
+//	    String end = System.console().readLine();
+//      int endnum = Integer.valueOf(end);
+       
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter end number: ");
+       String end = scanner.next();
+       int endnum = Integer.valueOf(end);
+       
+        FizzBuzz fizbuz = new FizzBuzz();
+		String[] strarr = fizbuz.fizzBuzz(snum, endnum);
+		for (int i = 0; i < strarr.length; i++) {
+			System.out.println(strarr[i]);
+		}
+	}
+
+	public String[] fizzBuzz(int start, int end) {
+		if (start > end) {
+			System.out.println("start is bigger than end");
+			// exit();
+		}
+
+		String[] strArray = new String[30];
+
+		for (int i = start; i <= end; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				strArray[i] = "FizzBuzz";
+			} else if (i % 3 == 0) {
+				strArray[i] = "Fizz";
+			} else if (i % 5 == 0) {
+				strArray[i] = "Buzz";
+			} else {
+				strArray[i] = String.valueOf(i);
+			}
+
+		}
+		return strArray;
+	}
+}
+
