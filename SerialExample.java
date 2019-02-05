318f660cbf3b74ca9fd518f7ec6357e0c6d1cc29
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-02-05 20:24:18 +0000

    Update SerialExample.java

--- SerialExample.java
+++ SerialExample.java
@@ -11,15 +11,14 @@
 import java.io.*; 
 
 class Emp implements Serializable { 
-private static final long serialversionUID = 
-								129348938L; 
+	private static final long serialversionUID = 129348938L; 
 	transient int a; 
 	static int b; 
 	String name; 
 	int age; 
 
 	// Default constructor 
-public Emp(String name, int age, int a, int b) 
+	public Emp(String name, int age, int a, int b) 
 	{ 
 		this.name = name; 
 		this.age = age; 
@@ -30,32 +29,27 @@ public Emp(String name, int age, int a, int b)
 } 
 
 public class SerialExample { 
-public static void printdata(Emp object1) 
+	public static void printdata(Emp object1) 
 	{ 
-
 		System.out.println("name = " + object1.name); 
 		System.out.println("age = " + object1.age); 
 		System.out.println("a = " + object1.a); 
 		System.out.println("b = " + object1.b); 
 	} 
 
-public static void main(String[] args) 
+	public static void main(String[] args) 
 	{ 
 		Emp object = new Emp("ab", 20, 2, 1000); 
 		String filename = "shubham.txt"; 
 
 		// Serialization 
 		try { 
-
 			// Saving of object in a file 
-			FileOutputStream file = new FileOutputStream 
-										(filename); 
-			ObjectOutputStream out = new ObjectOutputStream 
-										(file); 
+			FileOutputStream file = new FileOutputStream(filename); 
+			ObjectOutputStream out = new ObjectOutputStream(file); 
 
 			// Method for serialization of object 
 			out.writeObject(object); 
-
 			out.close(); 
 			file.close(); 
 
@@ -75,12 +69,9 @@ public static void main(String[] args)
 
 		// Deserialization 
 		try { 
-
 			// Reading the object from a file 
-			FileInputStream file = new FileInputStream 
-										(filename); 
-			ObjectInputStream in = new ObjectInputStream 
-										(file); 
+			FileInputStream file = new FileInputStream(filename); 
+			ObjectInputStream in = new ObjectInputStream(file); 
 
 			// Method for deserialization of object 
 			object = (Emp)in.readObject(); 
