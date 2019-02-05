61bb55e7ea3abf406246aa369d05f4c34f5cc151
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-02-05 20:34:36 +0000

    Update SerialExample.java

--- SerialExample.java
+++ SerialExample.java
@@ -1,12 +1,16 @@
 // Java code for serialization and deserialization of a Java object.
 
-//		1. If a parent class has implemented Serializable interface then child class doesn’t need to implement it 
-//		   but vice-versa is not true.
-//		2. Only non-static data members are saved via Serialization process.
-//		3. Static data members and transient data members are not saved via Serialization process.
+//	1. If a parent class has implemented Serializable interface then child class doesn’t need to implement it 
+//	   but vice-versa is not true.
+//	2. Only non-static data members are saved via Serialization process.
+//	3. Static data members and transient data members are not saved via Serialization process.
 //		   So, if you don’t want to save value of a non-static data member then make it transient.
-//		4. Constructor of object is never called when an object is deserialized.
-//		5. Associated objects must be implementing Serializable interface.
+//	4. Constructor of object is never called when an object is deserialized.
+//	5. Associated objects must be implementing Serializable interface.
+
+// Serialization is a mechanism of converting the state of an object into a byte stream. 
+// Deserialization is the reverse process where the byte stream is used to recreate the actual Java object in memory. 
+// This mechanism is used to persist the object.
 
 import java.io.*; 
 
