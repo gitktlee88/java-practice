56907d458d27f6fd807057a0ee54fcd856da77f3
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-02-06 12:52:58 +0000

    Update CallByValueCallbyReference.java

--- CallByValueCallbyReference.java
+++ CallByValueCallbyReference.java
@@ -1,10 +1,14 @@
-//•	Pass by Value: The method parameter values are copied to another variable(referrence) and then the copied object is passed.
-//•	Pass by Reference:
-// When we declare a reference type variable, the compiler allocates only space where the memory address of the object can be stored. 
+
+//•	Pass by Value: The method parameter values are copied to another variable(referrence) 
+//                     and then the copied object is passed.
+//•	Pass by Reference: When we declare a reference type variable, 
+// the compiler allocates only space where the memory address of the object can be stored. 
 // The space for the object itself isn't allocated. 
 // The space for the object is allocated at the time of object creation using the new keyword.
-// A variable of reference type differs from a variable of a primitive type in the way that a primitive type variable holds the actual data 
-// while a reference type variable holds the address of the object which it refers to and not the actual object. 	
+
+// A variable of reference type differs from a variable of a primitive type 
+// in the way that a primitive type variable holds the actual data, 
+// while a reference type variable holds the pointer(address of the object) which it refers to and not the actual object. 	
 
 class Number {
 	int x;
