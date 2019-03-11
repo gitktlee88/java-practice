c6c147f902afceea89702ef882ad499a31dd1c46
gitktlee88 <45231152+gitktlee88@users.noreply.github.com>
2019-03-11 14:25:32 +0000

    Update EuroMill.java

--- EuroMill.java
+++ EuroMill.java
@@ -20,9 +20,10 @@ public class EuroMill extends JFrame implements ActionListener
     public EuroMill(String str)
     {
         super(str);
-        lbl = new JLabel("버튼을 click 하세요.");
-        btn1 = new JButton("첫번째 버튼");
-        btn2 = new JButton("두번째 버튼");
+        
+        lbl = new JLabel("click the button.");
+        btn1 = new JButton("button 1");
+        btn2 = new JButton("버튼 2");
           // 버튼1,2를 ActionListener 사용하기
         btn1.addActionListener(this);
         btn2.addActionListener(this);
@@ -34,61 +35,65 @@ public class EuroMill extends JFrame implements ActionListener
         add("Center", panel);
         add("South", lbl);
         
-        setSize(300, 300);
+        setSize(450, 350);
         setVisible(true);
     }
     
    // 이벤트 발생시 행동하는 함수
-    public void actionPerformed(ActionEvent e) 
-    {
+   public void actionPerformed(ActionEvent e) 
+   {
         Object obj = e.getSource();
         if((JButton)obj == btn1) 
         {
             lbl.setText("First button clicked.");
-            int[] numArray = guessNums(1, 50);
+            int[] numArray = guessNums(1, 50, 5);
             String strArray[] = new String[numArray.length];
 
-    		for (int i = 0; i < numArray.length; i++)
-    			strArray[i] = String.valueOf(numArray[i]);
+    		      for (int i = 0; i < numArray.length; i++)
+    			         strArray[i] = String.valueOf(numArray[i]);
 
-    		System.out.println(Arrays.toString(strArray));
+    		      System.out.println(Arrays.toString(strArray));
             lbl.setText(Arrays.toString(strArray));
             
         }
         else     {
-            lbl.setText("두번째 버튼을 눌렀습니다.");
+            lbl.setText("Second button clicked.");
+            int[] numArray = guessNums(1, 12, 2);
+            String strArray[] = new String[numArray.length];
+
+    		      for (int i = 0; i < numArray.length; i++)
+    			         strArray[i] = String.valueOf(numArray[i]);
+
+    		      System.out.println(Arrays.toString(strArray));
+            lbl.setText(Arrays.toString(strArray));
         }
     }
     
-    public int[] guessNums(int min, int max){    	
+    public int[] guessNums(int min, int max, int cnt){    	
     	
-    	int[] nums = new int[5]; 
+    	int[] nums = new int[cnt]; 
     	Random rand = new Random();
     	
-    	for (int i = 0; i <5; i++){
-    		//int a = (int) (Math.random()*50 + 1);
-    		double d = Math.random()*((max - min) + 1);
-    		//System.out.println(d);
+    	for (int i = 0; i <cnt; i++){
+    		   //int a = (int) (Math.random()*50 + 1);    		
+    		   int a = min + (int)(Math.random()*((max - min) + 1));
     		
-    		int a = min + (int)(d);
+    		   //boolean contains = Arrays.stream(values).anyMatch("s"::equals);    		
+    		   boolean contains = IntStream.of(nums).anyMatch(x -> x == a);
     		
-    		//boolean contains = Arrays.stream(values).anyMatch("s"::equals);
-    		
-    		boolean contains = IntStream.of(nums).anyMatch(x -> x == a);
-    		
-    		if ( !contains){    			    			
-    			if (a != 0)	{
-    				//System.out.println(Integer.valueOf(a));
-    				//System.out.println(String.valueOf(a));
-    				//System.out.println(((Object)a).getClass().getName());
-    				nums[i] = a;
-    			}
-    		}
+    		   if ( !contains) {  
+				         //System.out.println(Integer.valueOf(a));
+				         //System.out.println(String.valueOf(a));
+				         //System.out.println(((Object)a).getClass().getName());
+				         nums[i] = a;
+    		   }
+    		   else {
+    			      i -= 1;			
+    		   }
     	
     	}	
     	
-    	//Arrays.sort(nums);
-    	System.out.println((int)(25.018927355080212));
+    	Arrays.sort(nums);
     	return nums;
     }
     
@@ -96,6 +101,5 @@ public class EuroMill extends JFrame implements ActionListener
     {
         new EuroMill("ActionEvent 테스트");
     }
-    
     
 }
